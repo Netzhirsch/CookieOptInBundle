a3c12746346b0de6363c005eb94a5c01ea24c822
Alexander Luhmann <luhmann@netzhirsch.de>
2020-03-04 12:32:06 +0100

    CCOI-128 unnötigen DependencyInjection Ordner löschen

--- src/NetzhirschCookieOptInBundle.php
+++ src/NetzhirschCookieOptInBundle.php
@@ -3,16 +3,7 @@
 namespace Netzhirsch\CookieOptInBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Netzhirsch\CookieOptInBundle\DependencyInjection\Extension;
 
 class NetzhirschCookieOptInBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getContainerExtension()
-    {
-        return new Extension();
-    }
-    
 }
