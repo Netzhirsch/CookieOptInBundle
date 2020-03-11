47f84d2b964e587bfce2d66c9a63397ee2d3e2ea
Alexander Luhmann <luhmann@netzhirsch.de>
2020-03-11 15:38:11 +0100

    Bugfix Facebook

--- src/Resources/public/netzhirschCookieOptIn.js
+++ src/Resources/public/netzhirschCookieOptIn.js
@@ -128,9 +128,7 @@
 					}
 					if (toolName.localeCompare('facebookPixel') === 0) {
 						<!-- Facebook Pixel Code -->
-					 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');/* Insert Your Facebook Pixel ID below. */fbq('init', tool.cookieToolsTrackingId);fbq('track', 'PageView');<!-- Insert Your Facebook Pixel ID below. -->
-					body.append('<img alt="Facebook Pixel" height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id='+tool.cookieToolsTrackingId+'&amp;ev=PageView&amp;noscript=1" />');
-					   <!-- End Facebook Pixel Code -->
+					 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', tool.cookieToolsTrackingId);fbq('track', 'PageView');
 					}
 					if (toolName.localeCompare('matomo') === 0) {
 						var _paq = window._paq || [];
