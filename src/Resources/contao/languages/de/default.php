a23094940c83e7392a22ef0b0996e3bdf1536c8f
luhmann <luhmann@netzhirsch.de>
2020-07-14 10:37:59 +0200

    CCOI-235 Readme Anpassung

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -37,7 +37,7 @@ $GLOBALS['TL_LANG']['FMD']['netzhirsch']['cookieOptIn'] = [
 ];
 $GLOBALS['TL_LANG']['BE_MOD']['netzhirsch']['cookieOptIn'] = [
 	'messages' => [
-		'contact' => 'Bitte kontaktieren Sie Netzhirsch unter <a href="tel:045138943740">0451 / 38 94 37 40</a> oder <a href="mailto:netzhirsch@netzhirsch.de">netzhirsch@netzhirsch.de</a>, um einen Lizenzschlüssel zu erwerben.',
+		'contact' => 'Bitte kontaktieren Sie Netzhirsch unter <a href="tel:045138943744">0451 / 38 94 37 44</a> oder <a href="mailto:cookies@netzhirsch.de">cookies@netzhirsch.de</a>, um einen Lizenzschlüssel zu erwerben.',
 		'domain' => 'Für Ihre Domain: ',
 		'errorTrial' => 'Der Probemonat für das Netzhirsch Cookie Opt In Bundle ist abgelaufen.',
 		'infoTrialStart' => 'Noch',
