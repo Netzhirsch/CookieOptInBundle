c15c2c8b1443861ebebc453d8d538e240949cdb3
Alexander Luhmann <luhmann@netzhirsch.de>
2023-12-20 08:22:41 +0100

    Cookie-Entscheidung bei Revoke nicht resetten.

--- src/Resources/public/js/NcoiRevoke.js
+++ src/Resources/public/js/NcoiRevoke.js
@@ -6,13 +6,29 @@ class NcoiRevoke {
     
     addOnClickEvent(storageKey) {
         let $ = this.$;
-        let ncoiCookie = new NcoiCookie($);
         $('.ncoi---revoke--button').on('click', function (e) {
             e.preventDefault();
+            let ncoiApp = new NcoiApp($);
+            let storage = ncoiApp.getLocalStorage(storageKey);
+            if (storage !== "") {
+                let cookieFields = $('[id^="ncoi---table-cookie-"]');
+                cookieFields.prop('checked',false);
+                storage.cookieIds.forEach(function (cookieId) {
+                    let cookieField = $('#ncoi---table-cookie-'+cookieId);
+                    cookieField.prop('checked',true);
+                });
+                $('[id^="group-"]').prop('checked',true);
+                cookieFields.each(function () {
+                    if (!$(this).prop('checked')) {
+                        $('#group-'+$(this).data('group')).prop('checked',false);
+                    }
+                });
+            }
+            let ncoiCookie = new NcoiCookie($);
             ncoiCookie.ajaxDeleteCookies(storageKey);
             ncoiCookie.removeCookies(Cookies.get());
             $('.ncoi---behind').removeClass('ncoi---hidden--page-load')
-                .removeClass('ncoi---hidden');
+            .removeClass('ncoi---hidden');
             $('#FBTracking').remove();
             $('#matomoTracking').remove();
         });
